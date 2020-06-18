c8304b8c10e384b438c802d80fbc14dbbcc50624
Manikant Rai <55659857+Manikant25@users.noreply.github.com>
2020-06-18 14:43:47 +0530

    Update Tower_of_Honoi.cpp

--- Recrusion_SourceCode/Tower_of_Honoi.cpp
+++ Recrusion_SourceCode/Tower_of_Honoi.cpp
@@ -15,3 +15,25 @@ int main()
  TOH(4,1,2,3);
  return 0;
 }
+
+/* The Towers of Hanoi is a mathematical puzzle. It consists of three rods (or pegs or
+towers), and a number of disks of different sizes which can slide onto any rod. The puzzle starts
+with the disks on one rod in ascending order of size, the smallest at the top, thus making a conical
+shape. The objective of the puzzle is to move the entire stack to another rod, satisfying the
+following rules:
+ • Only one disk may be moved at a time.
+• Each move consists of taking the upper disk from one of the rods and sliding it onto
+another rod, on top of the other disks that may already be present on that rod.
+• No disk may be placed on top of a smaller disk. */
+
+/* Algorithm
+
+• Move the top n – 1 disks from Source to Auxiliary tower,
+• Move the n
+th disk from Source to Destination tower,
+• Move the n – 1 disks from Auxiliary tower to Destination tower.
+• Transferring the top n – 1 disks from Source to Auxiliary tower can again be thought
+of as a fresh problem and can be solved in the same manner. Once we solve Towers
+of Hanoi with three disks, we can solve it with any number of disks with the above
+algorithm.
+*/

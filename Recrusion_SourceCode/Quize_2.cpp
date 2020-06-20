942af30e55280196140f650b17005392a095ffd4
Manikant Rai <55659857+Manikant25@users.noreply.github.com>
2020-06-20 20:28:18 +0530

    Add files via upload

---
+++ Recrusion_SourceCode/Quize_2.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+ void foo(int n,int sum)
+ {
+     int k=0,j=0;
+     if(n==0)
+     return; //Means stop
+     k=n%10;
+     j=n/10;
+     sum=sum+k;
+     foo(j,sum);
+     cout<<k;
+ }
+
+ int main()
+ {
+     int a,sum=0;
+     cin>>a;
+     foo(a,sum);
+     cout<<sum;
+     return 0;
+ }
+

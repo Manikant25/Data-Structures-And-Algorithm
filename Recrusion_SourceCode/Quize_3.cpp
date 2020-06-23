5e4cb35784e8e0311256d141cc887afae738eaec
Manikant Rai <55659857+Manikant25@users.noreply.github.com>
2020-06-24 02:50:44 +0530

    Add files via upload

---
+++ Recrusion_SourceCode/Quize_3.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int f(int &x,int c)
+{
+    c=c-1;
+    if(c==0)
+    return 1;
+    x=x+1;
+    return f(x,c)*x;
+}
+
+int main()
+{
+    int a;
+    cin>>a;
+    cout<<f(a,a);
+    return 0;
+}
